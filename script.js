387b28ebfd03b177649acc5181a5c78612608767
Whispering Code <74275864+luuizpeedro@users.noreply.github.com>
2025-05-22 18:10:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+particlesJS("particles", {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
+            }
+        },
+        opacity: {
+            value: 0.8,
+            random: true,
+            animation: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+        }
+    },
+    interactivity: {
+        detectsOn: "canvas",
+        events: {
+            onHover: {
+                enable: true,
+                mode: "push"
+            },
+            onClick: {
+                enable: true,
+                mode: "push"
+            },
+            resize: true
+        },
+        modes: {
+            repulse: {
+                distance: 100,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            }
+        }
+    },
+    retina_detect: true
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+    });
+}
+
+window.addEventListener('scroll', function () {
+    var scrollTopButton = document.querySelector('.scroll-top');
+    if (this.window.pageYOffset > 200) {
+        scrollTopButton.style.display = 'block';
+    } else {
+        scrollTopButton.style.display = 'none';
+    }
+});
+
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
+
+const track = document.querySelector('.carousel-track');
+let interval = setInterval(() => rotateCarousel(1), 4000); // autoplay
+
+function rotateCarousel(direction) {
+    const slides = Array.from(track.children);
+
+    if (direction === 1) {
+        track.appendChild(slides[0]);
+    } else {
+        track.insertBefore(slides[slides.length - 1], slides[0]);
+    }
+
+    updateActiveSlide();
+}
+
+// Destaque a imagem central
+function updateActiveSlide() {
+    const slides = Array.from(track.children);
+    const middleIndex = Math.floor(slides.length / 2);
+
+    slides.forEach(slide => slide.classList.remove('active'));
+    if (slides[middleIndex]) {
+        slides[middleIndex].classList.add('active');
+    }
+}
+
+// Executa no carregamento inicial
+updateActiveSlide();
